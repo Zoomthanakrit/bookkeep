fa8c0ad9716b4276ca16e705c5b6383542552e82
Zoomthanakrit <zoomthanakrit@gmail.com>
2026-03-11 15:32:21 +0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BookKeep from './bookkeep.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<BookKeep />)
